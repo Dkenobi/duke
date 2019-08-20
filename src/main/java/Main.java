8c40039db6a971fd7b1834d98dceb015b64883d0
Dillen <dillenwong@gmail.com>
2019-08-20 08:22:58 +0800

    Shifted Duke main code to DukeText. Refactor initialisation of Duke class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Duke duke = new Duke();
+    private DukeTest duke = new DukeTest();
 
     @Override
     public void start(Stage stage) {
