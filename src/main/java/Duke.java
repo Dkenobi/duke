86513cb229e8f96d716407b5364dcf09b80bf672
Dkenobi <dillenwong@gmail.com>
2019-08-22 17:44:22 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String name;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         printLine();
         printWelcomeMessage();
         printLine();
+
+        while(!(name = reader.readLine()).isEmpty()) {
+            if (name.equals("bye")){
+                echo("Bye. Hope to see you again soon!");
+                System.exit(0);
+            }
+            else
+                echo(name);
+        }
     }
     private static void printWelcomeMessage(){
         String logo = " ____        _        \n"
@@ -18,5 +35,10 @@ public class Duke {
 
     private static void printLine(){
         System.out.println("____________________________________________________________");
+    }
+
+    private static void echo(String string){
+        System.out.println(string);
+        printLine();
     }
 }
