f30d29bcf68a4b6ff34758235002a49abe671a25
Dkenobi <dillenwong@gmail.com>
2019-08-22 17:25:33 +0800

    Added welcome message and created a line seperator

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
+        printLine();
+        printWelcomeMessage();
+        printLine();
+    }
+    private static void printWelcomeMessage(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+    }
+
+    private static void printLine(){
+        System.out.println("____________________________________________________________");
     }
 }
