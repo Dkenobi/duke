1a2ef2358b68290ce95ab8ad45220f3ae05a0b8b
Dkenobi <dillenwong@gmail.com>
2019-08-26 21:38:01 +0800

    Changed icon to unicode.

--- src/main/java/expection/DukeException.java
+++ src/main/java/expection/DukeException.java
@@ -4,25 +4,25 @@ public class DukeException {
 
     public static class ToDoExceptionDuke extends DukeBaseException {
         public ToDoExceptionDuke() {
-            super("☹ OOPS!!! The description of a todo cannot be empty.");
+            super("\u2639 OOPS!!! The description of a todo cannot be empty.");
         }
     }
 
     public static class DeadlineException extends DukeBaseException{
         public DeadlineException() {
-            super("☹ OOPS!!! The description of a deadline cannot be empty.");
+            super("\u2639 OOPS!!! The description of a deadline cannot be empty.");
         }
     }
 
     public static class EventException extends DukeBaseException{
         public EventException() {
-            super("☹ OOPS!!! The description of a deadline cannot be empty.");
+            super("\u2639 OOPS!!! The description of a deadline cannot be empty.");
         }
     }
 
     public static class WrongCommandException extends DukeBaseException{
         public WrongCommandException() {
-            super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            super("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
     }
 }
