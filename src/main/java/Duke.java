87a1854b129e183b94cb841cc60102e8008659d0
Dkenobi <dillenwong@gmail.com>
2019-08-26 20:41:02 +0800

    Remove unused import

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
 import model.Deadlines;
 import model.Event;
-import model.Task;
 import model.ToDo;
 
 import java.io.BufferedReader;
@@ -8,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
