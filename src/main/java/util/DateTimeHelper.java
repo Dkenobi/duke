10ce0d94aaa9bb3f9345a39c6e3e06520e82f2eb
Dkenobi <dillenwong@gmail.com>
2019-08-28 13:55:24 +0800

    Add expection to return orignal time and date format if unable to parse.

--- src/main/java/util/DateTimeHelper.java
+++ src/main/java/util/DateTimeHelper.java
@@ -8,40 +8,42 @@ import java.util.Date;
 public class DateTimeHelper {
     // Convert 2/12/2019 1800 to 2nd of December 2019, 6pm
     public String getFormattedDate(String date) {
-        SimpleDateFormat inputFormat = new SimpleDateFormat("dd/M/yyyy HHmm");
-        Date formattedDate = new Date();
-
         try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("dd/M/yyyy HHmm");
+            Date formattedDate = new Date();
             formattedDate = inputFormat.parse(date);
+
+            int hour = Integer.parseInt(new SimpleDateFormat("h").format(formattedDate));
+            int min = Integer.parseInt(new SimpleDateFormat("m").format(formattedDate));
+            String period = (new SimpleDateFormat("a").format(formattedDate)).toLowerCase();
+            String timeFormat;
+
+            if (min > 0)
+                timeFormat = hour + "." + min + " " + period;
+            else
+                timeFormat = hour + " " + period;
+
+            Calendar cal=Calendar.getInstance();
+            cal.setTime(formattedDate);
+            int day=cal.get(Calendar.DATE);
+
+            if(!((day>10) && (day<19)))
+                switch (day % 10) {
+                    case 1:
+                        return new SimpleDateFormat("d'st' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
+                    case 2:
+                        return new SimpleDateFormat("d'nd' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
+                    case 3:
+                        return new SimpleDateFormat("d'rd' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
+                    default:
+                        return new SimpleDateFormat("d'th' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
+                }
+            return new SimpleDateFormat("d'th' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
+
         } catch (ParseException e) {
-            System.out.println(e.getMessage());
+            //If function is unable to parse return the original format.
+            System.out.println("Unable to parse date original date format is used.");
+            return date;
         }
-
-        int hour = Integer.parseInt(new SimpleDateFormat("h").format(formattedDate));
-        int min = Integer.parseInt(new SimpleDateFormat("m").format(formattedDate));
-        String period = (new SimpleDateFormat("a").format(formattedDate)).toLowerCase();
-        String timeFormat;
-
-        if (min > 0)
-            timeFormat = hour + "." + min + " " + period;
-        else
-            timeFormat = hour + " " + period;
-
-        Calendar cal=Calendar.getInstance();
-        cal.setTime(formattedDate);
-        int day=cal.get(Calendar.DATE);
-
-        if(!((day>10) && (day<19)))
-            switch (day % 10) {
-                case 1:
-                    return new SimpleDateFormat("d'st' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
-                case 2:
-                    return new SimpleDateFormat("d'nd' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
-                case 3:
-                    return new SimpleDateFormat("d'rd' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
-                default:
-                    return new SimpleDateFormat("d'th' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
-            }
-        return new SimpleDateFormat("d'th' 'of' MMMM yyyy").format(formattedDate) + " " + timeFormat;
     }
 }
