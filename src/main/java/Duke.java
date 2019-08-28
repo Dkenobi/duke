ea5d1628113fd54cf18197e67e1f8198fb2cb64e
Dkenobi <dillenwong@gmail.com>
2019-08-28 14:00:22 +0800

    Change those import all to a specific class used.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,10 +6,12 @@ import model.ToDo;
 import util.ConstantHelper;
 import util.DateTimeHelper;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
