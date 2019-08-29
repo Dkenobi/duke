ac0d26d4603b0fe3f99f00e2d7b98a2563b26617
Dkenobi <dillenwong@gmail.com>
2019-08-29 15:49:17 +0800

    [Refactor] Move TaskList to model package.

---
+++ src/main/java/model/TaskList.java
@@ -0,0 +1,56 @@
+package model;
+
+import model.Task;
+import util.ConstantHelper;
+import util.ReadWriteFileHelper;
+
+import java.util.ArrayList;
+
+public class TaskList {
+
+    private ArrayList<Task> tasksList;
+    private ArrayList<String> tasksSaveAsList;
+
+    public TaskList(){
+        tasksList = new ArrayList<>();
+        tasksSaveAsList = new ArrayList<>();;
+    }
+
+    public int addTaskToList(Task task){
+        tasksList.add(task);
+        tasksSaveAsList.add(task.saveToFile());
+        new ReadWriteFileHelper().saveFile(ConstantHelper.dukeFilePath,tasksSaveAsList);
+        return tasksList.size() - 1;
+    }
+
+    public String getTaskList(){
+        StringBuilder listString;
+        int index = 1;
+
+        listString = new StringBuilder("Here are the tasks in your list.");
+        for (var task: tasksList){
+            listString.append("\n").append(index++).append(".").append(task.toString());
+        }
+        return listString.toString();
+    }
+
+    public String completeTask(int index) {
+        int zeroIndex = index - 1;
+
+        tasksList.get(zeroIndex).setIsDone(true);
+        return "Nice I've marked this task as done: \n"
+                + "[" + tasksList.get(zeroIndex).getStatusIcon() + "] "
+                + tasksList.get(zeroIndex).getDescription();
+    }
+    public int getListSize(){
+        return tasksList.size();
+    }
+
+    public Task getTask(int id){
+        return tasksList.get(id);
+    }
+
+    public ArrayList<String> asList(){
+        return tasksSaveAsList;
+    }
+}
