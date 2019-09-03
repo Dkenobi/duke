27beb4c1ddf82f329d469552c30ebe8eb65692da
Dkenobi <dillenwong@gmail.com>
2019-09-03 19:36:02 +0800

    Added a few more commands in the shell and used gradlew build to compile Duke into a jar file.

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,18 +12,20 @@ then
     rm ACTUAL.TXT
 fi
 
+cd .. 
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if ! ./gradlew build
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -jar ./build/libs/duke-0.1.3.jar < ./text-ui-test/input.txt > ./text-ui-test/ACTUAL.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED.txt
+diff ./text-ui-test/ACTUAL.TXT ./text-ui-test/EXPECTED.txt
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
