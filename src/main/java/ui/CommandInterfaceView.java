d2f65237c2a89a85df2096d3e533c356c9cd9400
Dkenobi <dillenwong@gmail.com>
2019-09-04 21:00:42 +0800

    [Refactor] Remove unused import

--- src/main/java/ui/CommandInterfaceView.java
+++ src/main/java/ui/CommandInterfaceView.java
@@ -2,9 +2,6 @@ package ui;
 
 import model.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
