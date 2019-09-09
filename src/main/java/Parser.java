a85352ea7c8abc9453e321340d30b507b50009df
Dkenobi <dillenwong@gmail.com>
2019-09-09 22:40:08 +0800

    Added javadocs for Parser class.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,8 +6,17 @@ import util.DateTimeHelper;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * The Parser class help to determine the action along with the the additional argument to
+ * determine the command to execute.
+ */
 public class Parser {
-
+    /**
+     * This method determine the command to be execute base on the input given
+     * @param fullCommand the user input at runtime.
+     * @return the action to be executed base on the input given.
+     * @throws DukeBaseException throws and error when a wrong command is entered.
+     */
     public static Command parse(String fullCommand) throws DukeBaseException {
         String[] splitString = fullCommand.split("\\s+", 2);
 
@@ -28,6 +37,13 @@ public class Parser {
         }
     }
 
+    /**
+     * Similar to the above function however, this function only handle the various task
+     * to be executed.
+     * @param message the input after the command
+     * @return the action to be executed base on the input given.
+     * @throws DukeBaseException throws and error if missing information is entered.
+     */
     private static Command addTaskToList(String message) throws DukeBaseException {
         String[] splitString = message.split("\\s+");
         ArrayList<String> descriptionArgs = new ArrayList<>(Arrays.asList(splitString));
