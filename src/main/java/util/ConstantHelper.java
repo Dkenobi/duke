adf19d7adc17043f3bb0c853063a1b02cc7ea3fe
Dkenobi <dillenwong@gmail.com>
2019-09-09 22:41:18 +0800

    Added javadocs for ConstantHelper class.

--- src/main/java/util/ConstantHelper.java
+++ src/main/java/util/ConstantHelper.java
@@ -1,6 +1,8 @@
 package util;
 
+/**
+ * The ConstantHelper define the constant required in this application.
+ */
 public class ConstantHelper {
     public static final String dukeFilePath = "src/main/java/resource/duke.txt";
-
 }
